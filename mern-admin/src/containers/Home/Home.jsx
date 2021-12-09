82e8f7886aa929604a3293e3bb8d8773b1961e11
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-12-09 09:08:53 -0300

    Home modificado en funcion del loggeo

--- mern-admin/src/containers/Home/Home.jsx
+++ mern-admin/src/containers/Home/Home.jsx
@@ -1,20 +1,34 @@
 import { Container, Row, Col } from "react-bootstrap"
+import { useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 import './Home.css'
 
 export const Home = () => {
+    const auth = useSelector(state => state.auth)
     return (
         <div>
             <Container fluid>
-            <Row>
-                <Col md={2} className='sideBar'>
-                    <li> <NavLink to='/' className='w-75 text-decoration-none d-flex flex-start fs-1'>Home</NavLink></li>
-                    <li> <NavLink to='/products' className='w-75 text-decoration-none d-flex flex-start fs-1'>Products</NavLink></li>
-                    <li> <NavLink to='/products' className='w-75 text-decoration-none d-flex flex-start fs-1'>Orders</NavLink></li>
-                </Col>
-                <Col md={10} style={{marginLeft:'auto'}}>Container</Col>
-            </Row>
-        </Container>
+                {
+                    auth.token === null ?
+                        (
+                            <Col>
+                                <h1 className='d-flex justify-content-center'>Welcome !</h1>
+                                <h6 className='d-flex justify-content-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, suscipit illum! Delectus quis laudantium similique sint odio expedita tenetur tempore modi, error sunt nisi architecto quidem quaerat? Eaque, eligendi rem.</h6>
+                            </Col>
+                        ) :
+                        (
+
+                            <Row>
+                                <Col md={2} className='sideBar'>
+                                    <li> <NavLink to='/' className='text-decoration-none fs-4'>Home</NavLink></li>
+                                    <li> <NavLink to='/products' className='text-decoration-none fs-4'>Products</NavLink></li>
+                                    <li> <NavLink to='/products' className='text-decoration-none fs-4'>Orders</NavLink></li>
+                                </Col>
+                                <Col md={10} style={{ marginLeft: 'auto' }}>Container</Col>
+                            </Row>
+                        )
+                }
+            </Container>
         </div>
     )
 }
