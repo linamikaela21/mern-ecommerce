0dd89947d19b99b544e3bc5124365b9d0b84b557
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-12-28 13:34:00 -0300

    Ahora se pueden ver productos en el front

--- mern-frontend/src/containers/ProductsListPage/ProductsListPage.jsx
+++ mern-frontend/src/containers/ProductsListPage/ProductsListPage.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from 'react-router-dom'
 import { getProductsBySlug } from "../../redux/actions"
 import { ViewProductsListPage } from "./ViewProductsListPage"
@@ -7,8 +8,17 @@ import { ViewProductsListPage } from "./ViewProductsListPage"
 export const ProductsListPage = () => {
 
     const dispatch = useDispatch()
+    const products = useSelector(state => state.products)
 
     const { slug } = useParams()
+
+    const [priceRange, setPriceRange] = useState({
+        under5k: 5000,
+        under10k: 10000,
+        under15k: 15000,
+        under20k: 20000,
+        under30k: 30000,
+    })
     
     useEffect(() => {
         console.log(slug);
@@ -16,7 +26,11 @@ export const ProductsListPage = () => {
     }, [dispatch, slug])
     return (
         <div>
-            <ViewProductsListPage />
+            <ViewProductsListPage 
+            products={products} 
+            slug={slug}
+            priceRange={priceRange}
+            />
         </div>
     )
 }
--- mern-frontend/src/containers/ProductsListPage/ViewProductsListPage.jsx
+++ mern-frontend/src/containers/ProductsListPage/ViewProductsListPage.jsx
@@ -1,7 +1,41 @@
-export const ViewProductsListPage = () => {
+import './ProductsListPage.css'
+
+export const ViewProductsListPage = (props) => {
     return (
-        <div>
-            ViewProductsListPage
-        </div>
+        <>
+            {
+                Object.keys(props.products.productsByPrice).map((key, index) => {
+                    return (
+                        <div className="card">
+                            <div className="cardHeader">
+                                <div>{props.slug} under {props.priceRange[key]}</div>
+                                <button>View All</button>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                {
+                                    props.products.productsByPrice[key].map(prod => {
+                                        return (
+                                            <>
+                                                <div className="cardBody">
+                                                    <div className="cardImg"><img src={prod.image[0]} alt="" /></div>
+                                                </div>
+                                                <div className="cardInfo">
+                                                    <div style={{ margin: '0.4rem 0' }}>{prod.name}</div>
+                                                    <div>
+                                                        <span>4.3</span>&nbsp;
+                                                        <span>26548</span>
+                                                    </div>
+                                                    <div className="cardPrice">${prod.name}</div>
+                                                </div>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
